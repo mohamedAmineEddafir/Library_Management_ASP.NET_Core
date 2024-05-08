e675109e7f669c66dce62650e811e9277a07cb1e
eddafir_Mohamed_Amine404 <eddafir.mohamed.amine01@gmail.com>
2024-05-08 18:02:26 +0100

    push dbcontext and program

---
+++ NerdLibe_library_ASP.NET/bib/Program.cs
@@ -0,0 +1,49 @@
+using bib.data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(
+    CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(option => {
+        option.LoginPath = "/Client/sign_in";
+        option.AccessDeniedPath = "/Client/Index";
+        option.ExpireTimeSpan = TimeSpan.FromMinutes(1);
+
+    });
+
+//connect to Sql Server
+builder.Services.AddDbContext<Dbcontext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Client}/{action=Index}/{id?}"
+);
+
+app.Run();
---
+++ NerdLibe_library_ASP.NET/bib/data/dbcontext.cs
@@ -0,0 +1,20 @@
+﻿using bib.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace bib.data
+{
+    public class Dbcontext : DbContext
+    {
+        public Dbcontext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Livre> Livre { get; set; }
+        public DbSet<Evente> Evente { get; set; }
+        public DbSet<Demande> Demande { get; set; }  
+        public DbSet<Utilisateur> Utilisateur { get; set; }
+        public DbSet<Emprunt> Emprunt { get; set;}
+
+    }
+}
